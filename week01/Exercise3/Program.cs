9b2b152e872dd75df2a09e2b386c6572e293d606
keith109 <reyik109@hotmail.com>
2026-03-11 00:54:37 -0300

    week 01

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.Write("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.Write("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
